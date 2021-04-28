de60c312c7592d55c2f03d8e7036d5edfa5c3771
Abdelmoniem Selim <Abdelmoniem_Selim@outlook.com>
2021-04-28 15:06:13 +0200

    changes

---
+++ UserManagementWithIdentity/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagementWithIdentity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required, MaxLength(100)]
+        public string FirstName { get; set; }
+        [Required, MaxLength(100)]
+        public string LastName { get; set; }
+        public byte[] ProfilePicture { get; set; }
+    }
+}
